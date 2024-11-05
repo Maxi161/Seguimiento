18edfafd494fd584af3c8a5a69a32c14759448a6
OGMaximo <carmaxi92@gmail.com>
2024-11-05 19:45:27 -0300

    intento de arreglo 2

--- seguidor/src/app/page.tsx
+++ seguidor/src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 import { useUserContext } from "@/context/user.context";
-import { IUser } from "@/interfaces/user.interfaces";
+// import { IUser } from "@/interfaces/user.interfaces";
 import FollowView from "@/ui/follow/FollowView";
 import Footer from "@/ui/footer/Footer";
 import ApplicationForm from "@/ui/form/FormSeguimiento";
 import Header from "@/ui/header/Header";
 import InfoProject from "@/ui/info/Info.";
-import UserList from "@/ui/user/UserList";
+// import UserList from "@/ui/user/UserList";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -15,7 +15,7 @@ export default function Home() {
   const [formAppVisible, setFormAppVisible] = useState(false);
   const router = useRouter()
   const {getUsers, loading, isLogged} = useUserContext()
-  const [users, setUsers] = useState<IUser[]>([])
+  // const [users, setUsers] = useState<IUser[]>([])
   const toggleView = () => {
     setFormAppVisible((prevShowLogin) => !prevShowLogin);
   };
@@ -24,11 +24,11 @@ export default function Home() {
     if (!loading && !isLogged) {
       router.push("/auth");
     }
-    const saveUsers = async () => {
-      setUsers(await getUsers());
-    }
+    // const saveUsers = async () => {
+    //   setUsers(await getUsers());
+    // }
 
-    saveUsers()
+    // saveUsers()
   }, [isLogged, loading, router, getUsers]);
 
   if (loading) return <div>Loading...</div>;
