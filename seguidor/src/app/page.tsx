129f0135f5f5495d62e8caa9c00ad5e16585ac71
OGMaximo <carmaxi92@gmail.com>
2024-11-05 19:38:00 -0300

    primer arreglo del front (propiedad posiblemente vacía)

--- seguidor/src/app/page.tsx
+++ seguidor/src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
         <section>
           <InfoProject />
         </section>
-        <UserList users={users}/>
+        <UserList users={users || []}/>
       </main>
       <Footer />
     </div>
