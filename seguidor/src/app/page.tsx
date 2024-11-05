1833ac3a8ec5aeb628f498fa6825aa3531ed70ca
OGMaximo <carmaxi92@gmail.com>
2024-11-05 20:23:25 -0300

    intento de arreglo 3

--- seguidor/src/app/page.tsx
+++ seguidor/src/app/page.tsx
@@ -1,24 +1,24 @@
 "use client"
 import { useUserContext } from "@/context/user.context";
 // import { IUser } from "@/interfaces/user.interfaces";
-import FollowView from "@/ui/follow/FollowView";
+// import FollowView from "@/ui/follow/FollowView";
 import Footer from "@/ui/footer/Footer";
-import ApplicationForm from "@/ui/form/FormSeguimiento";
+// import ApplicationForm from "@/ui/form/FormSeguimiento";
 import Header from "@/ui/header/Header";
 import InfoProject from "@/ui/info/Info.";
 // import UserList from "@/ui/user/UserList";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
   
-  const [formAppVisible, setFormAppVisible] = useState(false);
+  // const [formAppVisible, setFormAppVisible] = useState(false);
   const router = useRouter()
   const {getUsers, loading, isLogged} = useUserContext()
   // const [users, setUsers] = useState<IUser[]>([])
-  const toggleView = () => {
-    setFormAppVisible((prevShowLogin) => !prevShowLogin);
-  };
+  // const toggleView = () => {
+  //   setFormAppVisible((prevShowLogin) => !prevShowLogin);
+  // };
 
   useEffect(() => {
     if (!loading && !isLogged) {
@@ -38,7 +38,7 @@ export default function Home() {
       <Header />
       <main className="flex justify-center items-center flex-col">
         <section className="w-10/12 m-8 relative">
-        {formAppVisible ? <ApplicationForm toggleView={toggleView}/> : <FollowView toggleView={toggleView} />}
+        {/* {formAppVisible ? <ApplicationForm toggleView={toggleView}/> : <FollowView toggleView={toggleView} />} */}
         </section>
         <section>
           <InfoProject />
